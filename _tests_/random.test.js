e055cce4cc39479aec2b6cc649033fcac549a290
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-12-23 15:46:11 -0500

    get stats and inventory on player object

--- _tests_/random.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('@jest/globals');
-const checkIfEqual = require('../lib/random.js');
-
-test('gets random number between 1 and 10', () =>
-{
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
