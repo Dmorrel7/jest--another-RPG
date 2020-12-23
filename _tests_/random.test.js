f854104f54a158ac03cf598716060b7fb33fe1fa
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-12-22 19:52:05 -0500

    fixing

---
+++ _tests_/random.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('@jest/globals');
+const checkIfEqual = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () =>
+{
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+});
